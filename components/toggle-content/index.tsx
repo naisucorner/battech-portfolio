c5c32e5babdfc483436b82e899b21d8170be01a4
Shion <naisucorner@gmail.com>
2022-07-19 10:11:33 +0700

    Update road

---
+++ components/toggle-content/index.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+const ToggleContent = () => {
+  return <></>;
+};
+
+export default ToggleContent;
---
+++ components/toggle-content/styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Title = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+
+export const TitleLogo = styled.div``;
+
+export const TitleText = styled.div`
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 25px;
+  color: #000000;
+`;
+
+export const Body = styled.div`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 25px;
+  color: #000000;
+`;
