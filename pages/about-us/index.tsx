a97ed773a2115c09efa250d47462daac47462047
Shion <naisucorner@gmail.com>
2022-07-20 15:21:57 +0700

    Update about us

--- pages/about-us/index.tsx
+++ pages/about-us/index.tsx
@@ -1,10 +1,51 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useState } from "react";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import PageHeader from "../../components/page-header";
 
-import { Container } from "./styles";
+import {
+  Container,
+  Body,
+  Introduce,
+  Content,
+  IntroduceImage,
+  IntroduceContent,
+  IntroduceHeader,
+  IntroduceHeaderTitle,
+  IntroduceHeaderBody,
+  IntroduceBody,
+  Tabs,
+  Tab,
+  TabContent,
+  IntroduceButton,
+  IntroduceButtonIcon,
+  Vision,
+  VisionBody,
+  VisionContent,
+  VisionTitle,
+} from "./styles";
+
+const data = [
+  {
+    title: "Phòng họp ảo",
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.",
+  },
+  {
+    title: "Dự án NFT",
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.",
+  },
+  {
+    title: "Game & Ứng dụng",
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.",
+  },
+];
 
 const AboutUs: NextPage = () => {
+  const [tab, setTab] = useState(data[0].title);
+
+  const handleChangeTab = (value: string) => setTab(value);
+
   return (
     <Container>
       <Head>
@@ -12,6 +53,88 @@ const AboutUs: NextPage = () => {
       </Head>
 
       <PageHeader title="Giới Thiệu" />
+
+      <Body>
+        <Introduce>
+          <Content>
+            <IntroduceImage>
+              <img src="./images/introduce.jpg" />
+            </IntroduceImage>
+
+            <IntroduceContent>
+              <IntroduceHeader>
+                <IntroduceHeaderTitle>
+                  Giới thiệu về BATTECH
+                </IntroduceHeaderTitle>
+
+                <IntroduceHeaderBody>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged. It was popularized in the 1960s with
+                  the release of Leeriest sheets containing Lorem Ipsum
+                  passages, and more recently with desktop publishing software
+                  like Lauds.
+                </IntroduceHeaderBody>
+              </IntroduceHeader>
+
+              <IntroduceBody>
+                <Tabs>
+                  {data.map(({ title }) => (
+                    <Tab
+                      key={title}
+                      isActive={tab === title}
+                      onClick={() => handleChangeTab(title)}
+                    >
+                      {title}
+                    </Tab>
+                  ))}
+                </Tabs>
+
+                <TabContent>
+                  {data.find((item) => item.title === tab)?.body}
+
+                  <IntroduceButton>
+                    Xem thêm
+                    <IntroduceButtonIcon>
+                      <ArrowForwardIcon sx={{ fontSize: "20px" }} />
+                    </IntroduceButtonIcon>
+                  </IntroduceButton>
+                </TabContent>
+              </IntroduceBody>
+            </IntroduceContent>
+          </Content>
+        </Introduce>
+
+        <Vision>
+          <VisionContent>
+            <VisionTitle>Tầm nhìn và sứ mệnh</VisionTitle>
+            <VisionBody>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+              <br/>
+              Contrary to popular belief, Lorem Ipsum is not simply random text.
+              It has roots in a piece of classical Latin literature from 45 BC,
+              making it over 2000 years old. Richard McClintock, a Latin
+              professor at Hampden-Sydney College in Virginia, looked up one of
+              the more obscure Latin words, consectetur, from a Lorem Ipsum
+              passage, and going through the cites of the word in classical
+              literature, discovered the undoubtable source.
+            </VisionBody>
+          </VisionContent>
+        </Vision>
+      </Body>
     </Container>
   );
 };
--- pages/about-us/styles.ts
+++ pages/about-us/styles.ts
@@ -2,5 +2,164 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
   width: 100%;
+`;
+
+export const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Introduce = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 50px;
+  justify-content: center;
+`;
+
+export const IntroduceImage = styled.div``;
+
+export const IntroduceContent = styled.div`
+  width: 586px;
+`;
+
+export const IntroduceHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const IntroduceHeaderTitle = styled.div`
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 30px;
+  color: #000000;
+`;
+
+export const IntroduceHeaderBody = styled.div`
+  margin-top: 7px;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 23px;
+  color: #000000;
+`;
+
+export const IntroduceBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
+  width: 100%;
+`;
+
+export const Tabs = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+type TabProps = {
+  isActive: boolean;
+};
+
+export const Tab = styled.div<TabProps>`
+  padding: 12px;
+  flex: 1;
+  background: #ffffff;
+  border: ${({ isActive }) => (isActive ? "1px solid #d7d7d7" : "none")};
+  border-bottom: ${({ isActive }) =>
+    isActive ? "1px solid transparent" : "1px solid #d7d7d7"};
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 30px;
+  color: #000000;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TabContent = styled.div`
+  padding: 20px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 25px;
+  color: #000000;
+  background: #ffffff;
+  border: 1px solid #d7d7d7;
+  border-top: none;
+`;
+
+export const IntroduceButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  white-space: nowrap;
+  padding: 11px 39px;
+  border: 1px solid #316fdc;
+  border-radius: 5px;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 25px;
+  color: #000000;
+  position: relative;
+  margin-top: 30px;
+  width: fit-content;
+  padding-right: 20px;
+
+  &:hover {
+    background: #101010;
+    color: #fff;
+  }
+`;
+
+export const IntroduceButtonIcon = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 26px;
+`;
+
+export const Vision = styled.div`
+  height: 580px;
+  width: 100%;
+  background-image: url("./images/vision.jpg");
+  background-repeat: no-repeat;
+  background-position: right top;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+`;
+
+export const VisionContent = styled.div`
+  max-width: 1174px;
+  padding-top: 35px;
+  width: 100%;
+`;
+
+export const VisionTitle = styled.div`
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 30px;
+  color: #000000;
+`;
+
+export const VisionBody = styled.div`
+  width: 590px;
+  margin-top: 15px;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 20px;
+  color: #000000;
 `;
